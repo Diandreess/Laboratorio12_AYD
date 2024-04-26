f651807d1e503a22291afcf6f834fe3e41a1176f
DiegoBautista <dabautistac@correo.url.edu.gt>
2024-04-25 23:58:49 -0600

    Creación del proyecto

---
+++ PaginasTendencia/ModeloDatos/Pagina.cs
@@ -0,0 +1,22 @@
+﻿namespace ModeloDatos
+{
+    public class Pagina
+    {
+        public Pagina()
+        {
+            Titulo = String.Empty;
+            Contenido = String.Empty;
+            FechaPublicacion = String.Empty;
+            Tematicas = new List<string>();
+        }
+
+        public  string Titulo { get; set; }
+        
+        public string Contenido { get; set; }
+
+        public string FechaPublicacion { get; set; }
+
+        public  List<string> Tematicas { get; set; }
+
+    }
+}
---
+++ PaginasTendencia/ModeloDatos/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeloDatos
+{
+    internal class Usuario
+    {
+        public Usuario()
+        {
+            NombreUsuario = string.Empty;
+            CorreoElectronico = string.Empty;
+            TienePermiso = false;
+        }
+
+        public string NombreUsuario { get; set; }
+
+        private string CorreoElectronico { get; set; }
+
+        public bool TienePermiso { get; set; }
+    }
+}
